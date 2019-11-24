1f3f0e6a8381df922c0496a447ba0c9becf35ef1
Kelven Galvão <kelvengalvao@gmail.com>
2019-11-23 22:53:48 -0300

    Cleaning project

---
+++ lib/src/clock/clock_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ClockPage extends StatefulWidget {
+  @override
+  _ClockPageState createState() => _ClockPageState();
+}
+
+class _ClockPageState extends State<ClockPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}

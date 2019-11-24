9ccb44f2db14421fc03a376e509d29f989235f4e
Kelven Galvão <kelvengalvao@gmail.com>
2019-11-23 23:05:04 -0300

    Update time logic

--- lib/src/clock/clock_page.dart
+++ lib/src/clock/clock_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class ClockPage extends StatefulWidget {
@@ -6,13 +8,36 @@ class ClockPage extends StatefulWidget {
 }
 
 class _ClockPageState extends State<ClockPage> {
+  DateTime _now;
+  Timer _timer;
+
+  @override
+  void initState() {
+    _updateTime();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [],
       ),
     );
+  }
+
+  void _updateTime() {
+    setState(() {
+      _now = DateTime.now();
+    });
+    _timer = Timer(Duration(seconds: 1), _updateTime);
   }
 }
